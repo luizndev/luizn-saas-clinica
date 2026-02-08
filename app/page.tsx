d3a566ade555ccdbb8207e67a563b1d43971b205
Luis Eduardo Andrade <andrade.ltg@gmail.com>
2026-02-08 14:46:54 -0300

    chore: add shadn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="">
+    <div className="dark">
      <h1>Home</h1>
+     <Button>Button</Button>
     </div>
   );
 }
