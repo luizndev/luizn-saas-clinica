cc16a877a1b19599f51933f2964c1030c4c1c423
Luis Eduardo Andrade <andrade.ltg@gmail.com>
2026-02-08 16:01:01 -0300

    chore: add betterauth 2

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,6 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
-import { user } from "@/auth-schema";
-
 import { db } from "../db";
 
 export const auth = betterAuth({
