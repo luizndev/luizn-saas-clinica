457edfc0eb2ec0612e4c3e07d8e0a6e982cdac15
Luis Eduardo Andrade <andrade.ltg@gmail.com>
2026-02-08 16:35:30 -0300

    feat: add custom error message for existing user

--- services/use-register.ts
+++ services/use-register.ts
@@ -2,6 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 import { UseFormReturn } from 'react-hook-form';
+import { toast } from 'sonner';
 
 import { authClient } from '@/lib/auth-client';
 
@@ -28,6 +29,9 @@ export const useRegister = (form: UseFormReturn<RegisterData>) => {
       },
       onError: (error) => {
         console.error(error);
+        if (error.error.code === "USER_ALREADY_EXISTS_USE_ANOTHER_EMAIL") {
+          toast.error("Email já cadastrado");
+        }
       }
     });
   };
